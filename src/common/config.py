7aa68147a481ff7c253d7ed05d786f9aff04a93e
MAYINJA Joel <joelmayinja@icloud.com>
2026-09-11 13:08:12 +0300

    Add shared ingestion configuration

---
+++ src/common/config.py
@@ -0,0 +1,18 @@
+SOURCE_A_RAW_PATH = "data/raw/prices.csv"
+SOURCE_B_RAW_PATH = "data/raw/rainfall.csv"
+
+
+MARKET_COORDS ={
+
+    "Mukono" :(0.3539, 32.7553),
+    "Bwaise" :(0.3476, 32.5610),
+    "Nakasero" :(0.3233, 32.5789),
+    "Kasanga" :(0.2872, 32.6078) 
+}
+
+
+RAINFALL_START_DATE = "2020-01-01"
+RAINFALL_END_DATE = "2020-06-30"
+
+
+RAINFALL_URL = "https://archive-api.open-meteo.com/v1/archive"
