f40eb7f6314ae608e66c2c6850ac0ca538e15c41
MAYINJA Joel <joelmayinja@icloud.com>
2026-09-25 12:19:19 +0300

    Add pipeline entry point

---
+++ src/run_pipeline.py
@@ -0,0 +1,21 @@
+from src.common.logging_setup import get_logger
+
+
+
+
+logger = get_logger()
+
+
+def run_pipe():
+    logger.info("===Pipeline run started===")
+
+
+    logger.info("stage 1 :Ingesting source a")
+
+
+
+    retrun
+
+
+
+    if __name__=="__main__"
