ec8ab031377c959ebb868ed1c3d471759531108d
MAYINJA Joel <joelmayinja@icloud.com>
2026-09-25 13:21:15 +0300

    Document merge stage contract

--- src/transform/merge.py
+++ src/transform/merge.py
@@ -2,5 +2,5 @@
 import pandas as pd
 
 
-def merge_data(clean_df: pd.DataFrame, rainfall_df: pd.DataFrame) -> pd.DataFrame:
+def merge_data(clean_df, rainfall_df):
     return clean_df.merge(rainfall_df, on=["date", "market"], how="left", validate="many_to_one")
